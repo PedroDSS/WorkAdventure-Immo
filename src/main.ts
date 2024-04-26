3985bd2d3f5597c091b1dfd2063859233f27d874
Erwann Jouvet <vinistys@gmail.com>
2024-04-26 11:10:00 +0200

    test form insertion + adding missing tileset

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,24 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 // import { ActionMessage } from "@workadventure/iframe-api-typings";
+// import { UIWebsite } from "@workadventure/iframe-api-typings";
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
 let currentPopup: any = undefined;
 let noteWebsite: any;
+let insertForm: any;
+let triggerMessage: any;
+const json = '../test.json';
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
+    console.log('Player tags: ',WA.player.tags);
+
+    // Fonction pour lire le fichier JSON
+
     // let menu = WA.ui.registerMenuCommand('titre du menu', {iframe: "test", key: "houseMenu", allowApi: true});
     // const msg:ActionMessageOptions = {
     //         message: 'appuie sur le bouton espace mec', 
@@ -28,7 +35,6 @@ WA.onInit().then(() => {
     //             WA.chat.sendChatMessage("confirmed", "trigger message logic")
     //         }
     //     },
-
     // );
 
     WA.room.area.onEnter('clock').subscribe(() => {
@@ -46,24 +52,75 @@ WA.onInit().then(() => {
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
+        
         WA.room.area.onEnter('housesLayer').subscribe(() => {
-            const triggerMessage = WA.ui.displayActionMessage({
+            triggerMessage = WA.ui.displayActionMessage({
                 message: "press 'space' to confirm",
                 callback: () => {
                     WA.chat.sendChatMessage("confirmed", "trigger message logic")
                 }
             });
-            // menu.open();
-            WA.room.area.onLeave('housesLayer').subscribe(() => {
-                console.log('dehors');
-                triggerMessage.remove();
-                // menu.remove();
-            });
         });
-        // WA.room.area.onLeave('housesLayer').subscribe(() => {
-        //     console.log('dehors');
-        //     // menu.remove();
-        // });
+        WA.room.area.onLeave('housesLayer').subscribe(() => {
+            console.log('dehors');
+            triggerMessage.remove();
+        });
+
+        WA.room.area.onEnter("terminal").subscribe(async () => {
+            console.log("dedans");
+            // let jsonContent = load();
+            // console.log(jsonContent);
+            triggerMessage = WA.ui.displayActionMessage({
+                message: "Appuyer sur espace pour ouvrir le formulaire d'insertion de logement",
+                callback: () => {
+                    insertForm = WA.ui.website.open({
+                        url: "./src/form.html",
+                        position: {
+                            vertical: "middle",
+                            horizontal: "middle",
+                        },
+                        size: {
+                            height: "75%",
+                            width: "75%",
+                        },
+                    });
+                }
+            });
+
+            // Wait for the iframe to load completely
+            // await new Promise<UIWebsite>((resolve, reject) => {
+            //     insertForm.addEventListener('load', resolve);
+            // });
+            let test = document.getElementById('divFormGlobal');
+            // let insertFormHtml = document.getElementById('ui-website-' + insertForm.id);
+            console.log('ui-website-' + insertForm.id);
+            let insertFormId = 'ui-website-' + insertForm.id;
+            console.log(insertFormId);
+            let insertFormHtml = document.getElementById('ui-website-'+insertForm.id);
+            console.log(insertFormHtml);
+            // let allIframes = await WA.ui.website.getAll();
+            // let insertForm = allIframes.find(iframe => iframe.url === './src/html/form.html')!;
+            // console.log(insertForm.id)
+            // let insertFormId = document.getElementById('#' + insertForm.id);
+
+            // if(insertForm){
+            //     insertForm.close();
+            // }
+
+            // let fermerFormulaire = document.getElementById('fermerFormulaire');
+            // console.log(fermerFormulaire);
+            // let formDiv = document.getElementById('formDiv'); // Remplacez par l'ID de votre formulaire
+        
+            // fermerFormulaire.addEventListener('click', function() {
+            //     console.log("t'as cliqué");
+            //     // formDiv.style.display = 'none'; // Cache le formulaire
+            // });
+        });
+        WA.room.area.onLeave("terminal").subscribe(() => {
+            console.log("dehors");
+            triggerMessage.remove();
+            insertForm.close();
+        });
     }).catch(e => console.error(e));
 
 }).catch(e => console.error(e));
