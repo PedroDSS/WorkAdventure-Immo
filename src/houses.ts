e730baaa88a3075416fb3c0284a6b2f4b65b96b7
DA SILVA SOUSA Pedro <pedromiguel.dasilvasousa92@gmail.com>
2024-04-28 15:16:06 +0200

    Quick fix for deployement

--- src/houses.ts
+++ src/houses.ts
@@ -1,4 +1,4 @@
-import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+// import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
